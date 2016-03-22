b3871fdc819022c8491e2197a47e12e0e17a2ba0
MasterOfDragons <weintraun8@students.rowan.edu>
2016-03-21 20:05:19 -0400

    Added Store/UI content

---
+++ TowerDefenseStore/Scripts/Item.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+
+	public string itemName;
+	public Sprite image;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public string getName()
+	{
+		return name;
+	}
+
+	public Sprite getImage()
+	{
+		return image;
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ TowerDefenseStore/Scripts/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject spawnThing;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void spawn()
+	{
+		Instantiate (spawnThing);
+	}
+}
