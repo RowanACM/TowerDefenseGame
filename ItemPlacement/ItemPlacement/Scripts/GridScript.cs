74d34095b3797c4761b145ccb00c90034fc59343
Alexander Kulpin <alexandtoddy@gmail.com>
2016-03-23 22:00:46 -0400

    ItemPlacememnt
    Still need to make obj move on x and z axis when moved and have it stick
    to the grid as well as some extra small things after it

---
+++ ItemPlacement/ItemPlacement/Scripts/GridScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GridScript : MonoBehaviour {
+
+    public List<Transform> cells = new List<Transform>();
+    public Transform GreenCell;
+    public Transform RedCell;
+    public Vector3 Size;
+    public string SecretCode;
+    int c = 0;
+
+	void Start() {
+        CreateGrid();
+    }
+
+    void CreateGrid() {
+        for(int x = 0; x < Size.x; x++) {
+            for (int z = 0; z < Size.z; z++) {
+                c++;
+                string part = SecretCode.Substring(c, 1);
+                if(part.Equals("1")) {
+                    Instantiate(GreenCell, new Vector3(x, 0, z), Quaternion.identity);
+                }
+                else {
+                    Instantiate(RedCell, new Vector3(x, 0, z), Quaternion.identity);
+                }
+            }
+        }
+    }
+}
---
+++ ItemPlacement/ItemPlacement/Scripts/RedCellScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RedCellScript : MonoBehaviour {
+
+    private Color startColor;
+
+    void OnMouseEnter() {
+        startColor = GetComponent<Renderer>().material.color;
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    void OnMouseExit() {
+        GetComponent<Renderer>().material.color = startColor;
+    }
+}
