c117a388b4c92d20b1ac5420ccca073b389091f9
Alexander Mendelsohn <superparkourio@gmail.com>
2016-04-01 16:02:58 -0400

    Bullet Damage
    Made damage from bullets work with bug's health

--- TowerLogic/Bullet.cs
+++ TowerLogic/Bullet.cs
@@ -1,15 +1,15 @@
-﻿/*using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
 public class Bullet : MonoBehaviour {
 
     public Transform Tower;
     public Transform Target;
-    public float damage;
+    public int damage;
     public float ApproachingTarget = 1.0f;
     public float transition = 0.0f;
     public bool fired = false;
-    float range = 0;
+    float range = 5f;
 
 	
 	// Update is called once per frame
@@ -33,17 +33,26 @@ public class Bullet : MonoBehaviour {
         float distance = Vector3.Distance(transform.position, Target.position);
         if(distance <= range) // if bullet hits the target;
         {
-            DoDamage();
-            Destroy(gameObject); //destroy bullet;
+            DoDamage(Target, damage);
         }
     }
 
-    void DoDamage()  // Need to work with bug logic;
+    void DoDamage(Transform target, int damage)  // Need to work with bug logic;
     {
+		Health tHealth = target.GetComponent ("Health") as Health;
 
+		if (target.GetComponent("Armor") != null) {
+			Armor armorComponent = target.GetComponent ("Armor") as Armor;
+			tHealth.health -= (damage - armorComponent.armor < 0)? 0: (damage - armorComponent.armor);
+		} else {
+			tHealth.health -= damage;
+		}
+
+		tHealth.checkHealth ();
+		Destroy(gameObject); //destroy bullet;
     }
 
-    public void Targeting(Transform tower, Transform target, float damage) 
+    public void Targeting(Transform tower, Transform target, int damage) 
     {
         fired = true;
         Tower = tower;
@@ -52,4 +61,3 @@ public class Bullet : MonoBehaviour {
     }
 
 }
-*/
