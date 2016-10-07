f0d64d6a91eb9e115bc9e438c37dbfa6ceb2e508
CornyElephant <eldraco@mac.com>
2016-10-07 15:14:07 -0400

    Add destruction volume and effects.
    Destruction Volume script is attached to the volume in the Mixing UI
    and Placement level, allowing store items to be dragged in for
    destruction at runtime. A particle system death component spawns a
    specified particle system on an object’s destruction.

---
+++ StoreUI/Scripts/AutoDestroyParticleSystem.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoDestroyParticleSystem : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!GetComponent<ParticleSystem>().IsAlive()){
+			Destroy(this);
+		}
+	
+	}
+}
---
+++ StoreUI/Scripts/ParticleSystemDeath.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleSystemDeath : MonoBehaviour {
+
+	public ParticleSystem destructionEffect;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnDestroy(){
+		Instantiate (destructionEffect, this.transform.position, this.transform.rotation);
+	}
+}
