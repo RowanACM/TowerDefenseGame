8d823f52ff17bf24ddc4e907908dcddd4d030a41
CornyElephant <eldraco@mac.com>
2016-11-02 16:45:31 -0400

    Delete old locations of removed files

--- BugLogic/Movement.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public abstract class Movement : MonoBehaviour {
-
-	public Animation animationMovement;
-	public float animationDelay;
-	public float movementSpeed;
-}
--- TowerLogic/Tranq.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class Tranq : Tower {
-
-	//not sure about this
-}
